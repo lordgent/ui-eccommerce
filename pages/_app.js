c93d539a6fc71b718706550b30c97f4e759d03bd
lordgent <Lorjenx08@gmail.com>
2025-01-29 07:31:42 +0700

    add adsense

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { Provider } from 'react-redux';
 import store from '../store/store'
 import LoadingIndicator from "@/components/loading/loading_indicator";
+import Script from 'next/script';
+
 
 export default function App({ Component, pageProps }) {
 
@@ -12,6 +14,13 @@ export default function App({ Component, pageProps }) {
   }, []);
   
   return <Provider store={store}>
+    <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6762897385240636"
+        strategy="afterInteractive" // Script hanya dimuat setelah halaman dimuat
+        crossOrigin="anonymous"
+        onError={(e) => console.error('AdSense script failed to load', e)}
+      />
           <LoadingIndicator />
 
     <Component {...pageProps} />
