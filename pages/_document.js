beee3056d4397d77faf6a21c99da6ceab1b721e7
lordgent <Lorjenx08@gmail.com>
2025-01-29 07:35:21 +0700

    add adsense

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="google-adsense-account" content="ca-pub-6762897385240636"/>
       <body className="antialiased">
         <Main />
         <NextScript />
